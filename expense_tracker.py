ba1947019cf5d0398cc605b9542b466abcb49bc1
Abimbola Okuwobi <141374442+Samuwar@users.noreply.github.com>
2026-08-16 21:32:40 +0100

    Add files via upload

--- expense_tracker.py
+++ expense_tracker.py
@@ -3,60 +3,89 @@
 # VIEW TOTAL EXPENSE
 # MENU
 import time
-CLEAR = "\033[2J"
-expense_list = []
+import json
+
+CLEAR = "\033[2J"       #this clears the screen // print(CLEAR)
+file_path = "expense_tracker.json"      # File path of the file to record expense on local desktop
+
+#Open the file to read and save tasks and if it doesn't exist, create the file
+try:
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+except FileNotFoundError:
+    data = {"data": []}
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+expense_list = data["data"]
+
 def add_expense():
     print(CLEAR)
-    print("ADD EXPENSE".center(20), end="\n")
-    item = input("Enter Expense Item: ")
-    not_acceptable = True
-    while not_acceptable:
+    print("ADD EXPENSE".center(20), end="\n\n")     #Header for display
+    item = input("Enter Expense Item: ")        # Expense Item
+    while True:     # Loop until input is acceptable, Input must be integer
         try:
             price = int(input("Enter Expense Amount: "))
-            not_acceptable = False
+            break
         except ValueError: print("\n\n\nOnly Number is allowed\n\n")       
-    expense = {"item":item, "price":price, "date":time.ctime()}
-    expense_list.append(expense)
-    view_expense(True)
+    expense = {"item":item, "price":price, "date":time.ctime()}     # Add item, price and date together in a dict
+    expense_list.append(expense)        # Add the expense to the expense list
+    view_expense(True)      # Call view_expense(True), True -> Displays the expense list for only 2 seconds and then return to main menu
     return True
 
 
 def view_expense(add=False):
+    """View Expense List: """
     print(CLEAR)
-    print("Expense List".center(50))
-    print("{:<5}".format("S/N"), "{:<28}".format("| ITEM"), " {:<10}".format("| PRICE"), "{:<12}".format("| DATE"))
-    total_expense = 0
+    print("EXPENSE LIST".center(50))        # Title
+    print("{:<5}".format("S/N"), "{:<28}".format("| ITEM"), " {:<10}".format("| PRICE"), "{:<12}".format("| DATE"))     #Headers
+    total_expense = 0       # Initial total
     for index, item in enumerate(expense_list):
-        print("-"*73)
-        print("{:<5}".format(index+1), "{:<29}".format("| "+ item.get("item")), "{:<10}".format("| "+ str(item.get("price"))), "{:<12}".format("| " +item.get("date")))
-        total_expense+=item.get("price")
-    print("{:>23}".format("TOTAL EXPENSE: "), "{:>17}".format(total_expense)) 
-    if add == False: input("\n\nPress Enter key to go to Main Menu: ")
-    else:
+        print("-"*73)       # Divides the rows
+        print("{:<5}".format(index+1), "{:<29}".format("| "+ item.get("item")), "{:<10}".format("| "+ str(item.get("price"))), "{:<12}".format("| " +item.get("date")))     # Print the expense item, price and date and format them
+        total_expense+=item.get("price")        # Add price of item to initial total
+    print()     # Print empty space
+    print("{:>23}".format("TOTAL EXPENSE: "), "{:>17}".format(total_expense))   # Print total expense under
+     
+    if add == False:        # if view_expense is called from main menu
+        while True:
+            try:
+                option = int(input("\n\nSelect item to delete \n0. Main Menu: ")) - 1
+                if option <= -1: return
+                else:
+                    if input(f"\n\nAre you sure you want to delete this {expense_list[option]['item']} {expense_list[option]['price']}? (y/n): ").lower().strip() == 'y':
+                        item_name = expense_list[option]['item']
+                        expense_list.remove(expense_list[option])       # remove item from expense list
+                        input(f"{item_name} is deleted successfully \n\nPress Enter Key to continue to main menu: ")
+                    break
+            except ValueError: print("Only Number is allowed")
+            except IndexError: print("Select a number from the list of items")       
+    else:       # Go to Main menu after 2 seconds
         print("\n\n Main Menu Loading...")
         time.sleep(2) 
     print(CLEAR)
     return True
-    
+
 def main_menu():
     print(CLEAR)
-    print("MAIN MENU\n".center(15))
-    not_acceptable = True
-    while not_acceptable:
-        try:
+    print("MAIN MENU\n".center(15))     # print title
+    while True:     # Loops until input is acceptable
+        try:        # handle error if input is not integer
             option = int(input("1. ADD EXPENSE\n2. VIEW EXPENSES LIST \n0. QUIT\n\nSelect an option: "))
-            not_acceptable = False
+            break
         except ValueError: print("\n\n\nOnly Number is allowed\n")
-    if option == 1: return add_expense()
-    elif option == 2: return view_expense()
-    else:
+    if option == 1: return add_expense()    # if user inputs 1, go to add expense
+    elif option == 2: return view_expense()     # if user input = 2, go to view expense
+    else:       # if user input is not 1 or 2, quit programme
         print(CLEAR)
         print("Ending Program...")
         time.sleep(.5)
         print("Program ended\n\n\n")
         return False
         
-run = True
-while run:
-    run = main_menu()
+while True:
+    if main_menu() == False: break
+        
+    data_string = json.dumps(data, indent=4)
+    with open(file_path, 'w') as file:
+        file.write(data_string)
     
--- to_do_list.py
+++ to_do_list.py
@@ -1,68 +1,113 @@
 ######### TO DO LIST ###########
 import json
 import time
+from datetime import datetime
 
-my_task = []
-completed_task = []
+CLEAR = "\033[2J"   #this clears the screen // print(CLEAR)
+file_path = "to_do.json"    # File path of the file to record task on local desktop
+
+#Open the file to read and save tasks and if it doesn't exist, create the file
+try: 
+    with open(file_path, 'r', encoding="utf-8") as file: 
+        data = json.load(file)
+except FileNotFoundError:
+    data = {"data": []}
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+
+my_task = data['data']
 
 def add_task():
-    task = input("Enter task to do: ")
-    my_task.append({'Task':task,'Date':time.ctime(), "Completed":'No'})
-    print(json.dumps(my_task), end="\n\n\n\n\n")
-    return
-
-def view_task():
-    print("To Do List:")
-    for index, item in enumerate(my_task):
-        print(f"{index+1}. {item}")
-    print("0. Return to main menu")
-    not_acceptable = True
-    while not_acceptable:
-        try:
-            option = int(input("Select an item: "))-1
-            if option == -1: return
-            print(my_task[option],"\n\n1. Mark as done \n0. Return to Main Menu")
-            not_acceptable = False
-        except ValueError: print("Only number is allowed\n\n\n")
-        except IndexError: print("Select a number from the list")    
+    """Add New Task"""
+    print(CLEAR)
+    print("Add Task".center(20), end="\n\n")    #Header for display
+    task = input("Enter task to do: ")      # Task to be added
     
-    not_acceptable = True
-    while not_acceptable:
+    # My Schema is {task, date_added, completed, date_completed}
+    my_task.append({"Task":task,"Date_added":datetime.now().strftime("%d-%m-%Y %H:%M:%S"), "Completed":"No", "Date_completed":""})
+    print(CLEAR)
+    print(f"{task} is added successfully!")
+    input("Press Enter key to go to main menu ")   
+    return True
+
+def view_task(task_list, not_completed=True):
+    """View uncompleted Tasks"""
+    #Task list is either completed list or uncompleted list sent from the main menu
+    #not_completed = True for view_task()
+    #not_completed = False for completed_task()
+    print(CLEAR)
+    
+    # Loop until the input is acceptable
+    while True:
+        if not_completed:       #To do tasks
+            print("To Do List:".center(80))
+            print("{:<5}".format("S/N"), "{:<32}".format("| TASK"),"{:<12}".format('| COMPLETED'), "{:<24}".format("| DATE ADDED")) #Header
+            for index, item in enumerate(task_list):
+                print("-"*80)       #Print line to divide item rows
+                print("{:<5}".format(index+1), "{:<32}".format("| " + item.get('Task')), "{:<12}".format("| "+item.get('Completed')), "{:<24}".format("| " + item.get('Date_added')))       # Print items and format them 
+        else:       #Completed Tasks
+            print("Completed Tasks:".center(98))
+            print("{:<5}".format("S/N"), "{:<32}".format("| TASK"),"{:<12}".format('| COMPLETED'), "{:<24}".format("| DATE ADDED"), "{:<24}".format("| DATE COMPLETED"))        # Header
+            for index, item in enumerate(task_list):
+                print("-"*98)       # Print line to divide items into rows
+                print("{:<5}".format(index+1), "{:<32}".format("| " + item.get('Task')), "{:<12}".format("| "+item.get('Completed')), "{:<24}".format("| " + item.get('Date_added')), "{:<24}".format("| " + item.get('Date_completed')))       # Print items and format them
+            
+        print("\n\nselect a task to mark as completed or 0. Return to main menu")
         try:
-            option = int(input("Select an Option: "))
-            if option == 1:
-                my_task[option]['Completed'] = "Yes"
-                not_acceptable = False
-            else: return
+            option = int(input("Select an item: "))     # Select task to mark
+            if option == 0: return
+            else:
+                option = option - 1  
+                print(CLEAR)
+                print(f"{task_list[option]['Task']}\n")
+                break
+        except ValueError: print("Only number is allowed\n")
+        except IndexError: print("Select a number from the list")  
+          
+    #ADD FUNCTION TO DELETE TASK: SELECT TASK, THEN MARK OR DELETE TASK
+    while True:
+        try:
+            if not_completed:       # For to do task
+                option1 = int(input("1. Mark Task as Done \n2. Delete Task \n0. Return to Main Menu: "))    # Select task to mark or delete
+                if option1 == 1:
+                    task_list[option]['Completed'] = "Yes"
+                    task_list[option]['Date_completed'] = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+                    break
+                elif option1 == 2:
+                    my_task.remove(task_list[option])
+                    break
+                else: return
+            else:       # For completed tasks
+                option1 = int(input("1. Delete Task \n0. Return to Main Menu: "))
+                if option1 == 1:
+                    my_task.remove(task_list[option])
+                    break
+                else: return
         except ValueError: print("Only number is allowed\n\n\n")
-        except IndexError: print("Select a number from the list")    
-    return
-
-def completed_task():
-    print("Completed Task:\n\n")
-    for index, item in enumerate(completed_task):
-        print(f"{index+1}. {item}")
-    return
-
-
+        
 def main():
-    print("Main Menu\n\n1. Add Task \n2. View Task \n3. Completed Task")
+    """Main Menu"""
+    print(CLEAR)
+    print("Main Menu\n\n1. Add Task \n2. View Task \n3. Completed Task \n0. Quit")
+    to_do_task = [x for x in my_task if x['Completed'] == "No"]
+    completed_task = [x for x in my_task if x['Completed'] == "Yes"]
     try:
         option = int(input("Select an option: "))
         match option:
             case 1: add_task()
-            case 2: view_task()
-            case 3: completed_task()
+            case 2: view_task(to_do_task)
+            case 3: view_task(completed_task, False)
             case _: return False
     except ValueError: print("Only numbers is allowed")
     return True
 
-run = True
-while run:
-   run = main()
-   print("Returning to Main Menu...")
-   time.sleep(2)
-       
+while True:     # Keep running the programme until you quit
+    if main() == False:
+        print("Ending Program ...")
+        time.sleep(.5)
+        print(CLEAR)
+        break
+    data_string = json.dumps(data, indent=4)
+    with open(file_path, 'w') as file:
+        file.write(data_string)   
    
-
-
